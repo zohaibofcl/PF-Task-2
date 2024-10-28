ccd0060ac936e33c57eda87495afd756a857beed
zohaibofcl <zohaibofcl@gmail.com>
2024-10-28 20:21:07 +0500

    Add files via upload

---
+++ markdown isosceles triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows = 5;
+    for (int i = rows; i >= 1; i--) {
+        for (int j = rows; j > i; j--) {
+            cout << " ";
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) { 
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
